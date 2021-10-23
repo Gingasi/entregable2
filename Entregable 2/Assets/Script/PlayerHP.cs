ac379b06391bf260226029c48d57a25f1a03c037
Flora de la Cruz <91455093+Gingasi@users.noreply.github.com>
2021-10-23 18:24:50 +0200

    Entregable2

---
+++ Entregable 2/Assets/Script/PlayerHP.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class PlayerHP : MonoBehaviour
+{
+    [SerializeField] private bool isGameOver;
+    public int PlayerHp = 40;
+    public int damage = 60;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log($"{PlayerHp} - {damage} = {PlayerHp - damage}");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGameOver == false)
+        {
+            if (PlayerHp <= 0)
+            {
+                Debug.Log($"Te has quedado sin vida… GAME OVER");
+            }
+            else if (PlayerHp == 30)
+            {
+                Debug.Log($" ¡¡¡Tienes poca vida!!!");
+            }
+            else 
+            {
+                Debug.Log($"  Vas bien de vida: tienes {PlayerHp} puntos de vida");
+            }
+        }
+        if (isGameOver == true)
+        {
+                Debug.Log($"GAME OVER");
+        }
+    }
+}
+
